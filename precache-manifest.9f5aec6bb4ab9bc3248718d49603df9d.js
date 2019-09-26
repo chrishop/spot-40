7a0057e918663eed7bc34273a6508fe2a9f1f2af
chrishop <christopher.hopkins@bbc.co.uk>
2019-09-26 21:22:48 +0100

    Updates

---
+++ precache-manifest.9f5aec6bb4ab9bc3248718d49603df9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbefe82eb2133cc0e47d7e4598671d6",
+    "url": "/chrishop/spot-40/index.html"
+  },
+  {
+    "revision": "57c18838e82fd6803398",
+    "url": "/chrishop/spot-40/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c85fa794b86d12ab8c7a",
+    "url": "/chrishop/spot-40/static/js/2.08a732c4.chunk.js"
+  },
+  {
+    "revision": "57c18838e82fd6803398",
+    "url": "/chrishop/spot-40/static/js/main.955ef0b4.chunk.js"
+  },
+  {
+    "revision": "f5710dca40d149e5c2fe",
+    "url": "/chrishop/spot-40/static/js/runtime-main.d99604b9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/chrishop/spot-40/static/media/logo.25bf045c.svg"
+  }
+]);
